d9490362cb4240f1d04ecb14c02a37e45ae67f4b
JOHN ALI <116637847+john12gate@users.noreply.github.com>
2023-09-28 01:11:20 +0100

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,9 @@
 export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
-
   if (trueOrFalse) {
     let task = true; // Use 'let' to declare a block-scoped variable
     let task2 = false; // Use 'let' to declare a block-scoped variable
   }
-
   return [task, task2];
 }
-
