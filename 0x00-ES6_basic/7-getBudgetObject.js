8744ff046fe922bf74781835fec852503e03ba60
JOHN ALI <116637847+john12gate@users.noreply.github.com>
2023-10-01 12:12:50 +0100

    7-getBudgetObject.js

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,11 @@
+Modify the following function’s budget object to simply use the keyname instead.
+
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income: income,
+    gdp: gdp,
+    capita: capita,
+  };
+
+  return budget;
+}
