49f529fec651430fc606260225cec32f112e0037
JOHN ALI <116637847+john12gate@users.noreply.github.com>
2023-10-01 12:27:05 +0100

    101-iterateThroughObject.js

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const employeeNames = [];
+  for (const name of reportWithIterator) {
+    employeeNames.push(name);
+  }
+  return employeeNames.join(' | ');
+}
