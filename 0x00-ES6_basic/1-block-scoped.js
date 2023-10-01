85c1a4bf7912a4e49f4331539fe5d2c4bd5dc9cb
JOHN ALI <116637847+john12gate@users.noreply.github.com>
2023-10-01 11:58:16 +0100

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,8 +2,8 @@ export default function taskBlock(trueOrFalse) {
  let task = false;
  let task2 = true;
 if (trueOrFalse) {
- let task = true;
- let task2 = false;
+ var task = true;
+ var task2 = false;
  }
  return [task, task2];
  }
