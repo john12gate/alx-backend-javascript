1c42ef7fb08c914958bfc19ffbee8ebdccaed290
JOHN ALI <116637847+john12gate@users.noreply.github.com>
2023-10-01 13:28:59 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,25 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+function handleResponseFromAPI(promise) {
+  return promise
+    .then((response) => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch((error) => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
 
+// Usage example:
+const somePromise = /* Your promise here */;
+
+handleResponseFromAPI(somePromise)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
